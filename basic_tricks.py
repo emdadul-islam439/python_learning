eecf8a52f03183f66b48d2271ff2f1ea808fe3ff
Emdadul_Islam <emdadulislam439@gmail.com>
2022-07-27 15:00:53 +0600

    added two more tricks

--- basic_tricks.py
+++ basic_tricks.py
@@ -1,1 +1,8 @@
-print(10 * 'hello world ', end = '')
+# 'hello world' will be printed 10 times
+print(10 * 'hello world ')
+
+#this time it will actually multiply the numbers
+print(10 * int('79') + int('89'))
+
+#but this time the calculation will be printed 10 times
+print(10 * str(int('79') + int('89')))
