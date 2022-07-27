8b7893bc86c183f79d095f7142d71b685a2e5d4b
Emdadul_Islam <emdadulislam439@gmail.com>
2022-07-27 15:11:35 +0600

    input() method takes input as string only

---
+++ input_something.py
@@ -0,0 +1,9 @@
+#just taking input
+print('enter a number: ')
+inputNum1 = input()
+print('Your inputted value is: ', inputNum1)
+
+#doing operation with the input value
+print('enter another number: ')
+inputNum2 = input()
+print('Your inputted value + 10 = ', int(inputNum2) + 10)
