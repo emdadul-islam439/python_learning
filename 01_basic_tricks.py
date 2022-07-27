4135d0044e9b966db116ea8e8ff5bd97ddce3eb3
Emdadul_Islam <emdadulislam439@gmail.com>
2022-07-27 17:51:46 +0600

    practiced out-of-the-box SWAP feature, renamed some files.

--- 01_basic_tricks.py
+++ 01_basic_tricks.py
@@ -1,4 +1,7 @@
 # 'hello world' will be printed 10 times
+from re import A
+
+
 print(10 * 'hello world ')
 
 #this time it will actually multiply the numbers
@@ -6,3 +9,11 @@ print(10 * int('79') + int('89'))
 
 #but this time the calculation will be printed 10 times
 print(10 * str(int('79') + int('89')))
+
+
+a = 1
+b = 2
+print('(a,b) = ', '(', a, ',', b, ')')
+
+a,b = b,a 
+print('(a,b) = ', '(', a, ',', b, ')')
---
+++ 06_tuple_and_related_functions.py
@@ -0,0 +1,11 @@
+exampleTuple = ('hi', 'hello', 'how', 'are', 'you')
+print(exampleTuple)
+
+#must add comma after the item of the tuple if there is only one item in the tuple
+singleItem = ('who')
+print(singleItem)
+
+oneItemTuple = ('who',)
+print(oneItemTuple)
+
+
