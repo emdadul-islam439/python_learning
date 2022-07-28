c3e84f0feae899fd7cff13364304e44652c18e48
Emdadul_Islam <emdadulislam439@gmail.com>
2022-07-28 07:10:34 +0600

    did some while() loop related exercises

---
+++ 12_while_loop_exercise.py
@@ -0,0 +1,14 @@
+list1 = ['ami', 'tumi', 'he', 'she', 'it']
+
+i = 0
+while(i < len(list1)):
+    print(list1[i])
+    i = i + 1
+
+i = 0
+while(i < list1.len()):
+    print(list1[i])
+    i = i + 1
+
+
+
