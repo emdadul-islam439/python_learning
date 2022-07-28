c60867cfd619e6f5195eafa29a9f163309e31115
Emdadul_Islam <emdadulislam439@gmail.com>
2022-07-28 06:58:56 +0600

    found a library based solution for the exercise

--- 11_01_exercises_using_for_loop.py
+++ 11_01_exercises_using_for_loop.py
@@ -1,6 +1,5 @@
-from curses.ascii import isalnum, isdigit
-from operator import truediv
-
+#At first didn't get a library function like this. So, wrote my own function
+#(reference: question of https://stackoverflow.com/questions/354038/how-do-i-check-if-a-string-is-a-number-float)
 def is_int(given_input):
     try:
         int(given_input)
@@ -15,3 +14,10 @@ for item in exampleList:
     if is_int(item):
         if int(item) > 6:
             print(item)
+
+
+print('--------------------')
+## another library function based solution:
+for item in exampleList:
+    if str(item).isnumeric() and item > 6:
+        print(item)
