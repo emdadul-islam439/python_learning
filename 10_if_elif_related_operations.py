c99f341c8c15094b5d2318fe8e0433a9ca187a11
Emdadul_Islam <emdadulislam439@gmail.com>
2022-07-28 08:12:42 +0600

    practiced short-hand if-else statement

--- 10_if_elif_related_operations.py
+++ 10_if_elif_related_operations.py
@@ -19,3 +19,12 @@ if 3 in st:
     print('3 is in st')
 else:
     print('3 is not in st')
+
+
+#short-hand if-else related operation
+a = 5
+b = 6
+print('a is greater than b') if(a>b) else print('a is smaller or equal to b')
+
+## can't add elif between short-hand if-else
+print('a is greater than b') if(a>b) elif(a==b) print('a and b are equal')  else print('a is smaller or equal to b')
