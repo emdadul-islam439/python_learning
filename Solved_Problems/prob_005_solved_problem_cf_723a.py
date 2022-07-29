9c43a12ec82e5e89d47f46a6aedb06ef7d36357f
Emdadul_Islam <emdadulislam439@gmail.com>
2022-07-29 09:09:21 +0600

    solved the cf 723a problem.

--- Solved_Problems/prob_005_solved_problem_cf_723a.py
+++ Solved_Problems/prob_005_solved_problem_cf_723a.py
@@ -1,2 +1,6 @@
 a, b, c = map(int, input().split())
-print(a, b, c)
+minimum_step_needed = 300
+for i in range(101):
+    steps_needed = abs(i-a) + abs(i-b) + abs(i-c)
+    minimum_step_needed = min(minimum_step_needed, steps_needed)
+print(minimum_step_needed)
