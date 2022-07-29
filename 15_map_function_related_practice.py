7c66c3c83d407e441da1c943a0b8a7f22536e395
Emdadul_Islam <emdadulislam439@gmail.com>
2022-07-29 08:58:51 +0600

    Did some practices related to map() function, created files for solving the 005-th problem

--- 15_map_function_related_practice.py
+++ 15_map_function_related_practice.py
@@ -4,6 +4,7 @@ def myfunc(a):
 x = map(myfunc, ('apple', 'banana', 'cherry', 'berry'))
 
 print(x) #output: <map object at 0x.....>
+print((x)) #output: <map object at 0x.....>
 
 #convert the map into a list, for readability:
 print(list(x))  #output: [5, 6, 6, 5]
@@ -18,4 +19,16 @@ print(type({x})) #output: <class 'set'>
 
 #trying to convert into tuple
 print(tuple(x)) #output: ()
-print(str(x))
+
+#assigning values with map()
+a, b, c = map(int, input().split())
+print(a, b, c)
+
+#can we assign value to a list with map()?
+string = '1 2 33 50 60 70' #experimenting with string line
+emptyList = list(map(int, string.split()))
+print(emptyList)
+
+stringList = ['1', '2', '3', '55', '33'] #experimenting with map()?
+stringToIntList = list(map(int, stringList))
+print(stringToIntList)
