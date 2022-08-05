d024c57163a8385fdcbedb965931ce738a385ce4
Emdadul_Islam <emdadulislam439@gmail.com>
2022-08-05 23:28:10 +0600

    read the problem statement carefully, but another wrong answer

--- Solved_Problems/prob_021_solved_problem_cf_279b.py
+++ Solved_Problems/prob_021_solved_problem_cf_279b.py
@@ -2,13 +2,22 @@ n, t = map(int, input().split())
 arr = list(map(int, input().split()))
 arr.sort()
 
-print(arr)
-# count = 0
-# total_book = 0
+start_idx = 0
+time_count = 0
+max_book_read = 0
 
-# for time in arr:
-#     count += time
-#     if(count > t):
-#         break
-#     total_book += 1
-# print(total_book)
+for i in range(n):
+    if arr[i] > t:
+        time_count = 0
+        start_idx = i+1
+    else:
+        time_count += arr[i]
+        if(time_count <= t):
+            dif = i - start_idx + 1
+            max_book_read = max(max_book_read, dif)
+        else:
+            while(time_count > t):
+                time_count -= arr[start_idx]
+                start_idx += 1
+
+print(max_book_read)
